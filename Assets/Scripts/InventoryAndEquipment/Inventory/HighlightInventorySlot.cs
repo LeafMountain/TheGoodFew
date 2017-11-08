41c7eb895e7c5e261adbec81dd3ec53b909cf74f
Stenkrona <axelstenkrona@me.com>
2017-11-08 11:46:01 +0100

    Moved function in to new class
    Moved the content of InventorySlot.HighligtSelf(bool lit) into a new class called HighlightInventorySlot.

---
+++ Assets/Scripts/InventoryAndEquipment/Inventory/HighlightInventorySlot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightInventorySlot {
+
+    private InventorySlot inventorySlot;
+    private bool lit;
+
+    private Color highligtColor;
+    private Color normalColor;
+
+    public HighlightInventorySlot(
+        InventorySlot inventorySlot, bool lit)
+    {
+        this.inventorySlot = inventorySlot;
+        this.lit = lit;
+
+        highligtColor = Color.yellow;
+        normalColor = Color.white;
+
+        Highlight();
+    }
+
+    private void Highlight()
+    {
+        Image imageComponent =
+            inventorySlot.transform.GetChild(0).
+            gameObject.GetComponent<Image>();
+
+        if(lit)
+        {
+            imageComponent.color = highligtColor;
+        }
+        else
+        {
+            imageComponent.color = normalColor;
+        }
+    }
+}
--- Assets/Scripts/InventoryAndEquipment/Inventory/InventorySlot.cs
+++ Assets/Scripts/InventoryAndEquipment/Inventory/InventorySlot.cs
@@ -10,8 +10,7 @@ public class InventorySlot : MonoBehaviour
     private Image frame;
     private ShopManager shopManager;
     private BarracksManager barracksManager;
-    private Color highligtColor;
-    private Color normalColor;
+    
     private Item item;  // Current item in the slot
     private AbilityDataOffensive ability;
     private bool isAbilitySlot;
@@ -26,8 +25,7 @@ public class InventorySlot : MonoBehaviour
         {
             barracksManager = GetComponentInParent<BarracksManager>();
         }
-        highligtColor = Color.yellow;
-        normalColor = Color.white;
+      
         frame = GetComponent<Image>();
         if (gameObject.name[0] == 'A') {
             isAbilitySlot = true; }
@@ -107,17 +105,7 @@ public class InventorySlot : MonoBehaviour
     }
     public void HightlightSelf(bool lit)
     {
-        Image imageComponent =
-            transform.GetChild(0).gameObject.GetComponent<Image>();
-
-        if (lit)
-        {
-           imageComponent.color = highligtColor;   
-        }
-        else
-        {
-            imageComponent.color = normalColor;
-        }
+        new HighlightInventorySlot(this, lit);
     }
     //Properties
     public Item _Item { get { return item; } set { item = value; } }
